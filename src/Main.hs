69c24ff1cd2f0e9bda0e39cd9827f882dcb4f984
Sandy Maguire <sandy@sandymaguire.me>
2016-09-02 08:44:54 -0700

    clean up printf a little bit

--- src/Main.hs
+++ src/Main.hs
@@ -11,8 +11,8 @@ module Main where
 import Data.Proxy (Proxy (..))
 import GHC.TypeLits
 
-data (:>) (a :: k) (b :: k')
-infixr 9 :>
+data (:<<) (a :: k) (b :: k')
+infixr 9 :<<
 
 class HasPrintf a where
     type Printf a :: *
@@ -22,20 +22,18 @@ instance KnownSymbol s => HasPrintf s where
     type Printf s = String
     printf' s p = s ++ symbolVal p
 
-instance (Show t, HasPrintf rest) => HasPrintf ((t :: *) :> rest) where
-    type Printf (t :> rest) = t -> Printf rest
-    printf' s (_ :: Proxy (t :> rest)) = \v -> printf' (s ++ show v) (Proxy @rest)
+instance (Show t, HasPrintf rest) => HasPrintf ((t :: *) :<< rest) where
+    type Printf (t :<< rest) = t -> Printf rest
+    printf' s (_ :: Proxy (t :<< rest)) = \v -> printf' (s ++ show v) (Proxy @rest)
 
-instance (KnownSymbol s, HasPrintf rest) => HasPrintf (s :> rest) where
-    type Printf (s :> rest) = Printf rest
-    printf' s (_ :: Proxy (p :> rest)) = printf' (s ++ symbolVal (Proxy @p)) (Proxy @rest)
+instance (KnownSymbol s, HasPrintf rest) => HasPrintf (s :<< rest) where
+    type Printf (s :<< rest) = Printf rest
+    printf' s (_ :: Proxy (p :<< rest)) = printf' (s ++ symbolVal (Proxy @p)) (Proxy @rest)
 
 printf :: HasPrintf p => Proxy p -> Printf p
 printf = printf' ""
 
-x :: Proxy ("Hello" :> Int :> "Goodbye")
-x = Proxy
-
 main :: IO ()
-main = putStrLn $ printf x 7
+main = putStrLn
+     $ printf (Proxy @("Hello" :<< Bool :<< "Goodbye")) True
 
