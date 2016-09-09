81cb08454469d077c83a82583dff8ff7f7384099
Sandy Maguire <sandy@sandymaguire.me>
2016-09-08 23:31:13 -0700

    add `t a -> t (t a -> a)`

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,23 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Main where
 
-import Stories
+import Control.Monad.State (get, runState, modify)
+import qualified Data.Foldable as F
+
+-- import Stories
+
+loeb :: Traversable t
+     => t a
+     -> t (t a -> a)
+loeb ta = fst
+        . flip runState 0
+        . sequence
+        . flip fmap ta
+        . const $ do
+            here <- get
+            modify (+1)
+            return $ \ta' ->
+                F.toList ta' !! here
+
 
 main = putStrLn "hello"
