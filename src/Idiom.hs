e19ac5173d7b1b5e3ab11c32009b69e0d9b7cd37
Sandy Maguire <sandy@sandymaguire.me>
2016-09-16 10:41:55 -0500

    cleaning up yer mum

--- src/Idiom.hs
+++ src/Idiom.hs
@@ -12,7 +12,7 @@ module Idiom where
 import Data.Proxy
 
 class HasFnOfArity (ts :: [*]) where
-    type FnOfArity ts :: *
+    type FnOfArity                ts :: *
     type FFnOfArity (f :: * -> *) ts :: *
     idiom' :: Applicative f
           => Proxy ts
@@ -20,21 +20,21 @@ class HasFnOfArity (ts :: [*]) where
           -> FFnOfArity f ts
 
 instance HasFnOfArity (t ': '[]) where
-    type FnOfArity    (t ': '[]) = t
+    type FnOfArity    (t ': '[]) =   t
     type FFnOfArity f (t ': '[]) = f t
-    idiom' _ f = f
+    idiom' _ = id
 
 instance HasFnOfArity (t ': ts) => HasFnOfArity (a ': (t ': ts)) where
     type FnOfArity    (a ': (t ': ts)) = a -> FnOfArity    (t ': ts)
     type FFnOfArity f (a ': (t ': ts)) = a -> FFnOfArity f (t ': ts)
-    idiom' _ f = \a -> idiom' (Proxy @(t ': ts)) (f <*> pure a)
+    idiom' _ f a = idiom' (Proxy @(t ': ts)) (f <*> pure a)
 
 idiom :: forall f ts.
          ( HasFnOfArity ts
          , Applicative f
          )
-      => Proxy ts
-      -> FnOfArity ts
+      => Proxy        ts
+      -> FnOfArity    ts
       -> FFnOfArity f ts
 idiom p f = idiom' p (pure f :: f (FnOfArity ts))
 
