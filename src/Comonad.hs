56e5bb9030daef3ce30ded74bdfec6f9d837248b
Sandy Maguire <sandy@sandymaguire.me>
2017-01-12 23:20:26 -0800

    zipppppperrrs

--- src/Comonad.hs
+++ src/Comonad.hs
@@ -6,8 +6,6 @@ module Comonad where
 import Data.Function
 import Data.Semigroup
 import Control.Comonad
-import Control.Comonad.Store
-import Debug.Trace
 
 data Stream a = a :> Stream a deriving Functor
 
@@ -36,7 +34,7 @@ moveR (Zipper ls a (r :> rs)) = Zipper (a :> ls) r rs
 
 instance Comonad Zipper where
   extract (Zipper _ a _) = a
-  duplicate = zipperf moveL moveR
+  duplicate = zipper moveL moveR
 
 coiter :: (a -> a) -> a -> Stream a
 coiter f a = f a :> coiter f (f a)
@@ -47,11 +45,8 @@ instance ComonadApply Stream where
 instance ComonadApply Zipper where
   Zipper lf f rf <@> Zipper la a ra = Zipper (lf <@> la) (f a) (rf <@> ra)
 
-zipperf :: (a -> a) -> (a -> a) -> a -> Zipper a
-zipperf lf rf a = Zipper (coiter lf a) a (coiter rf a)
-
-zipper :: a -> a -> a -> Zipper a
-zipper l a r = Zipper (coiter id l) a (coiter id r)
+zipper :: (a -> a) -> (a -> a) -> a -> Zipper a
+zipper lf rf a = Zipper (coiter lf a) a (coiter rf a)
 
 stolist :: Stream a -> [a]
 stolist (a :> as) = a : stolist as
@@ -60,10 +55,10 @@ tolist :: Zipper a -> [a]
 tolist (Zipper _ a as) = stolist $ a :> as
 
 wat :: Zipper (Zipper Integer -> Integer)
-wat = zipperf id (\f w -> extract (moveL w) + extract (moveL $ moveL w)) (const 1)
+wat = zipper id (\f w -> extract (moveL w) + extract (moveL $ moveL w)) (const 1)
 
 sheet :: Sheet (Sheet Integer -> Integer)
-sheet = Embed . zipperf id id $ zipperf id (const go) $ const 1
+sheet = Embed . zipper id id $ zipper id (const go) $ const 1
   where
     go :: Sheet Integer -> Integer
     go (unembed -> w) = up + left
@@ -71,14 +66,14 @@ sheet = Embed . zipperf id id $ zipperf id (const go) $ const 1
         up   = extract . extract $ moveL w
         left = extract . moveL $ extract w
 
-pascal :: Sheet Integer
-pascal = kfix sheet
+pascal :: Zipper (Zipper Integer)
+pascal = unembed $ kfix sheet
 
 moveRN :: Integer -> Zipper a -> Zipper a
 moveRN (fromIntegral -> n) = head . drop n . iterate moveR
 
 triangle :: Integer -> Integer
-triangle n = extract $ moveRN n . extract . moveRN n $ unembed pascal
+triangle n = extract $ moveRN n . extract . moveRN n $ pascal
 
 
 main :: IO ()
